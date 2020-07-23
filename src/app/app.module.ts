1db5d6b7e83233e5244792929edb9bb4afd68730
ryanKrumbholz <krumbholz98@gmail.com>
2020-07-23 19:30:17 -0400

    Updated table ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { MatInputModule} from '@angular/material/input';
+import { MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    FormComponent,
+    SearchbarComponent,
+    CreateUserComponent,
+    DisplayUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+export interface Experience {
+  title: string;
+  desc: string;
+  date: Date;
+}
+
+export interface EmploymentHistory {
+  employerName: string;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  status: boolean;
+  desc: string;
+}
+
+export interface User {
+  name: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+  age: Number;
+  // experience: Experience;
+  // employmentHistory: EmploymentHistory;
+}
+
+const USER_DATA: User[] = [{
+  name: 'Ryan K', 
+  address: 'Washington, DC', 
+  phoneNumber: '206-427-5234', 
+  email: 'krumbholz98@gmail.com',
+  age: 21
+},
+{
+  name: 'test', 
+  address: 'test', 
+  phoneNumber: 'test', 
+  email: 'test',
+  age: 1
+}];
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.sass']
 })
-export class TableComponent implements OnInit {
+
+export class TableComponent implements OnInit{
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  displayedColumns: string[] = ['name', 'address',
+   'email', 'phoneNumber', 'age'];
+  dataSource = USER_DATA;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
